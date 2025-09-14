edecf37fabc1019346120c207e49eed64facea59
Silas Curtis <142943737+SilasEC@users.noreply.github.com>
2025-09-14 14:19:27 -0500

    Update file_explorer.sh

--- file_explorer.sh
+++ file_explorer.sh
@@ -1,3 +1,10 @@
+#file_explorer.sh
+#Silas Curtis
+#CS 310
+#Due 9/15/25
+#This script loops through a specified directory and prints the names and some basic characteristics of the items within it.
+# If no directory is specified, the current one is used. File type and size are not printed for directories.
+
 #!/bin/bash
 
 dir="${1:-.}"   
